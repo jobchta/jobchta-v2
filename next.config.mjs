07dd99c5e991b9d1faaf94645da592205db46179
jobchta <byte4bits4@gmail.com>
2025-07-07 20:04:23 -0400

    fix: Reset next.config.mjs and clear Vercel cache [vercel skip]

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // This setting tells Next.js to only consider files ending in ".page.tsx"
-  // as part of the old Pages Router. Since you have no files with this
-  // extension, it effectively disables it and forces the build to
-  // only use your `app` directory.
-  pageExtensions: ['page.tsx'],
-};
+const nextConfig = {};
 
 export default nextConfig;
